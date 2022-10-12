f25c6eebf82addbed5251a1f2f2d67fb3f9acc3e
Saifuddinmonna <Saifuddinmonna@gmail.com>
2022-10-12 14:57:40 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Navbar,Nav,Container  } from "react-bootstrap";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<h2 className="border shadow p-7  text-start ">
+				amar assignment start{" "}
+			</h2>
+			
+			<Navbar bg="primary" variant="dark">
+				<Container>
+					<Navbar.Brand href="#home">Navbar</Navbar.Brand>
+					<Nav className="me-auto">
+						<Nav.Link href="#home">Home</Nav.Link>
+						<Nav.Link href="#features">Features</Nav.Link>
+						<Nav.Link href="#pricing">Pricing</Nav.Link>
+					</Nav>
+				</Container>
+			</Navbar>
+
+		
+		</div>
+	);
 }
 
 export default App;
