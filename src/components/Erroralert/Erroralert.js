e49b6f3a966030ebc1891ee20f56fe33d70d6907
Saifuddinmonna <Saifuddinmonna@gmail.com>
2022-10-12 23:24:37 +0600

    error correction modified

--- src/components/Erroralert/Erroralert.js
+++ src/components/Erroralert/Erroralert.js
@@ -1,12 +1,12 @@
-import React from 'react';
+import React from "react";
 
 const Erroralert = () => {
-        return (
-                <div>
-                        <h2>Wrning!</h2>
-                        <h3>Your router is not correct!</h3>
-                </div>
-        );
+	return (
+		<div className="container text-center text-red-700">
+			<h2>Wrning!</h2>
+			<h3>Your router Path is not correct!</h3>
+		</div>
+	);
 };
 
 export default Erroralert;
